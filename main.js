eecf2259f563f6bf43a97bb3ad55de0d593916f6
Ashton Fei <yunjia.fei@gmail.com>
2020-07-26 00:56:21 +0800

    initial release

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const uid_key = "uid"   // uid key for generating the sequential registration number
-const uid_prefix = ""   // uid prefix
-const uid_length = 4    // uid digit length
-const _max_responses = 1500 // max reseponses allowed
+const uid_prefix = "UID-"   // uid prefix
+const uid_length = 5    // uid digit length
+const _max_responses = 99999 // max reseponses allowed
+const _relations = "Relations"
+const _regions = "Regions"
+const _provinces = "Provinces"
+const _cities = "Cities"
+const _hospitals = "Hospitals"
+const _sep = ","
 
 function onOpen(e){
     let ui = SpreadsheetApp.getUi()
-    ui.createMenu("CSR Form")
+    ui.createMenu("App")
         .addItem("Open form", "openWebApp")
         .addItem("Reset form", "resetForm")
         .addToUi()
@@ -25,11 +31,12 @@ function include(filename){
  * @param {event object} e 
  */
 function doGet(e){
+    let title = "Custom Form for Fitri"
     let template = HtmlService.createTemplateFromFile("index.html")
     let htmlOuput = template.evaluate()
     htmlOuput
       .addMetaTag("viewport", "width=device-width,initial-scale=1,minimal-ui")
-      .setTitle("Pendaftaran Solat Aidiladha & Solat Jumaat 31 Jul 2020")
+      .setTitle(title)
       .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
     return htmlOuput   
 }
@@ -99,4 +106,41 @@ function openWebApp(){
     let html = `<script>window.open("${url}");google.script.host.close();<\/script>`
     let userInterface = HtmlService.createHtmlOutput(html).setTitle("Opening the web app...")
     SpreadsheetApp.getActive().show(userInterface)
+}
+
+function getAppData(){
+    let ws = SpreadsheetApp.getActive().getSheetByName(_relations)
+    let values = ws.getDataRange().getValues()
+    
+    let regions = []
+    let rObject = {}
+    
+    let provinces = []
+    let pObject = {}
+    
+    let cities = []
+    let cObject = {}
+    
+    let hospitals = []
+    let hObject = {}
+    
+    
+    values.forEach(([region, province, city, hospital], i)=>{
+        if (i > 0){
+            region = region.trim()
+            province = province.trim()
+            city = city.trim()
+            hospital = hospital.trim()
+            
+            let rKey = region.toLowerCase().replace(" ", "-")
+            let pKey = rKey + "_" + province.toLowerCase().replace(" ", "-")
+            let cKey = pKey + "_" + city.toLowerCase().replace(" ", "-")
+            let hKey = cKey + "_" + hospital.toLowerCase().replace(" ", "-")
+            rObject[rKey] = {name: region, id: rKey}
+            pObject[pKey] = {name: province, id: pKey, parent: rKey}
+            cObject[cKey] = {name: city, id: cKey, parent: pKey}
+            hObject[hKey] = {name: city, id: hKey, parent: cKey}
+        }
+    })
+    return JSON.stringify({rObject, pObject, cObject, hObject})
 }
